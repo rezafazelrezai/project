69504a1afdb5750a83be076946ba4839f8ba8c70
Reza Fazel-Rezai <rezafazelrezai@gmail.com>
2020-04-06 16:40:09 -0700

    commit_2

--- code.m
+++ code.m
@@ -19,14 +19,14 @@ fprintf('z_a2= %0.4f,  z1_a2=%0.4f,  z2_a2=%0.4f,  th1_a2=%0.4f,  th2_a2=%0.4f, 
     z_a2, z1_a2, z2_a2, th1_a2, th2_a2, m_a2, k_a2)
 
 
-function [z1,z2,th1,th2] = myfun1(z,m,k)
-
-th1=atand(1/m);
-th2=atand(2*cotd(2*th1));
-z1=m*z;
-z2=z1/k;
-
-end
+% function [z1,z2,th1,th2] = myfun1(z,m,k)
+% 
+% th1=atand(1/m);
+% th2=atand(2*cotd(2*th1));
+% z1=m*z;
+% z2=z1/k;
+% 
+% end
 % 
 % function [z1, z2, m, th2] = myfun2(z,th1,k)
 % 
